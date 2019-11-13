290ec5dc28ad5384eba1b670e74313209f8616ba
Farts-Jousta <49703765+Farts-Jousta@users.noreply.github.com>
2019-11-13 15:06:19 +1100

    started frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Router from './Router';
+import './App.css'
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router />
+    );
+  };
+};
+
+export default App;
---
+++ frontend/src/Router.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+const Router = () => {
+  return (
+    <Switch>
+    </Switch>
+  );
+};
+
+export default Router;
